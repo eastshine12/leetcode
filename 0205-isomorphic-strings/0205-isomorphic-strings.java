ed9834a81d9df13e2f7f150b2ed8384605bfe2fd
DongHyun Kim <75498839+eastshine12@users.noreply.github.com>
2024-04-02 09:35:16 +0900

    Time: 17 ms (23.31%), Space: 42.4 MB (51.51%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        
+        Map<Character, Character> map1 = new HashMap<>();
+        Map<Character, Character> map2 = new HashMap<>();
+        
+        for(int i = 0; i < s.length(); i++) {
+            char sChar = s.charAt(i);
+            char tChar = t.charAt(i);
+            
+            if(map1.containsKey(sChar)) {
+                if(map1.get(sChar) != tChar) {
+                    return false;
+                }
+            } else {
+                map1.put(sChar, tChar);
+            }
+            
+            if(map2.containsKey(tChar)) {
+                if(map2.get(tChar) != sChar) {
+                    return false;
+                }
+            } else {
+                map2.put(tChar, sChar);
+            }
+        }
+        
+        return true;
+    }
+}
